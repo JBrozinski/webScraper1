dc1bb459b8627e8bf81c16a2dd69a6ae0bed10a4
JBrozinski <joseph.brozinski@gmail.com>
2020-07-06 11:15:37 -0600

    deploying on heroku

--- server.js
+++ server.js
@@ -33,6 +33,13 @@ app.use(routes);
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/webScraper";
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build/'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI);
 
