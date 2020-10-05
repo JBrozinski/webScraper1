81cf4e5aa922ef7fee88e9a4c819da575e3ea513
JBrozinski <joseph.brozinski@gmail.com>
2020-10-05 17:12:34 -0600

    debugging article link href by updating url def in scrape function

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
     var cardHeader = $("<div class='card-header'>").append(
       $("<h3>").append(
         $("<a class='article-link' target='_blank' rel='noopener noreferrer'>")
-          .attr("href='https://bitcoinmagazine.com/articles'")
+          .attr("href", article.url)
           .text(article.headline)
         // $("<a class='btn btn-success save'>Save Article</a>")
       )
